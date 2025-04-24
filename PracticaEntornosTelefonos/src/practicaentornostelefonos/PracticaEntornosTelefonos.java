b312277f4fb032139e83172ea38e682ca99309d4
crlangas <carlospilarsanz@gmail.com>
2025-04-24 18:16:10 +0200

    Añado el link en los comentarios

--- PracticaEntornosTelefonos/src/practicaentornostelefonos/PracticaEntornosTelefonos.java
+++ PracticaEntornosTelefonos/src/practicaentornostelefonos/PracticaEntornosTelefonos.java
@@ -6,6 +6,8 @@ import java.util.Scanner;
 /**
  *
  * @author Carlos
+ *  *  @see <a href="https://view.genially.com/621e9aa9df21310011780bf1"> Link a el genially </a>
+
  */
 public class PracticaEntornosTelefonos {
 
